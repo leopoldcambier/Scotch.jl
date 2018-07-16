426a9a4074b5bf05f619dec5d6b15a64f71f381e
Leopold Cambier <leopoldcambier@gmail.com>
2018-07-16 10:48:27 -0700

    Defining libscotch outside source code

--- src/Scotch.jl
+++ src/Scotch.jl
@@ -1,7 +1,8 @@
 
 module Scotch
 
-const libscotch = "/Users/lcambier/Stanford/SCOTCH.jl/src/libscotch.dylib"
+# Define before execution, for instance in your /~.juliarc.jl
+# const SCOTCH_LIBSCOTCH = "/Users/lcambier/Stanford/SCOTCH.jl/src/libscotch.dylib"
 
 mutable struct Graph
 end
@@ -20,14 +21,14 @@ function version()
     ver = Cint[0]
     rel = Cint[0]
     pat = Cint[0]
-    ccall((:SCOTCH_version, libscotch), Void, 
+    ccall((:SCOTCH_version, SCOTCH_LIBSCOTCH), Void, 
             (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), 
             pointer(ver), pointer(rel), pointer(pat))
     return (ver[1], rel[1], pat[1])
 end
 
 function graphAlloc()
-    graph = ccall((:SCOTCH_graphAlloc, libscotch), Ptr{Graph}, ())
+    graph = ccall((:SCOTCH_graphAlloc, SCOTCH_LIBSCOTCH), Ptr{Graph}, ())
     if graph == C_NULL
         error("Error in graphAlloc")
     end
@@ -35,7 +36,7 @@ function graphAlloc()
 end
 
 function graphInit(graph::Ptr{Graph})
-    err = ccall((:SCOTCH_graphInit, libscotch), Cint, 
+    err = ccall((:SCOTCH_graphInit, SCOTCH_LIBSCOTCH), Cint, 
                 (Ptr{Graph},), 
                 graph)
     err == 0 || error("Error in graphInit")
@@ -48,7 +49,7 @@ function graphLoad(graph::Ptr{Graph}, filename, baseval::Integer, flagval::Integ
              (Ptr{Char}, Ptr{Char}),
              pointer(filename), pointer(m))
     file != C_NULL || error("Error in graphInit, fopen")
-    err = ccall((:SCOTCH_graphLoad, libscotch), Cint,
+    err = ccall((:SCOTCH_graphLoad, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Graph}, Ptr{File}, Cint, Cint),
                 graph, file, Cint(baseval), Cint(flagval))
     err == 0 || error("Error in graphInit")
@@ -75,7 +76,7 @@ function graphBuild(graph::Ptr{Graph}, A::SparseMatrixCSC{Tv, Ti}) where {Tv, Ti
         unsafe_store!(verttab, k, i+1)
     end
     # FIXME: resize edgetab to k
-    err = ccall((:SCOTCH_graphBuild, libscotch), Cint,
+    err = ccall((:SCOTCH_graphBuild, SCOTCH_LIBSCOTCH), Cint,
           (Ptr{Graph}, Cint, Cint,    Ptr{Cint},  Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Cint,    Ptr{Cint},  Ptr{Cint}),
           graph,       1,    vertnbr, verttab,    C_NULL,    C_NULL,    C_NULL,    edgenbr, edgetab,    C_NULL)
     err == 0 || error("Error in graphBuild")
@@ -83,7 +84,7 @@ function graphBuild(graph::Ptr{Graph}, A::SparseMatrixCSC{Tv, Ti}) where {Tv, Ti
 end
 
 function graphCheck(graph::Ptr{Graph})
-    err = ccall((:SCOTCH_graphCheck, libscotch), Cint, 
+    err = ccall((:SCOTCH_graphCheck, SCOTCH_LIBSCOTCH), Cint, 
                 (Ptr{Graph},), 
                 graph)
     err == 0 || error("Error in graphCheck")
@@ -92,20 +93,20 @@ end
 function graphSize(graph::Ptr{Graph})
     nv = Cint[0]
     ne = Cint[0]
-    ccall((:SCOTCH_graphSize, libscotch), Void,
+    ccall((:SCOTCH_graphSize, SCOTCH_LIBSCOTCH), Void,
                (Ptr{Graph}, Ptr{Cint}, Ptr{Cint}),
                graph, pointer(nv), pointer(ne)) 
     return (nv[1], ne[1])
 end
 
 function graphFree(graph::Ptr{Graph})
-    ccall((:SCOTCH_graphFree, libscotch), Void,
+    ccall((:SCOTCH_graphFree, SCOTCH_LIBSCOTCH), Void,
           (Ptr{Graph},),
           graph)
 end
 
 function graphExit(graph::Ptr{Graph})
-    ccall((:SCOTCH_graphExit, libscotch), Void,
+    ccall((:SCOTCH_graphExit, SCOTCH_LIBSCOTCH), Void,
           (Ptr{Graph},),
           graph)
 end
@@ -114,7 +115,7 @@ function graphStat(graph::Ptr{Graph})
     A1 = Cint[0,0,0,0,0,0,0,0]
     A2 = Cdouble[0,0,0,0,0,0]
     @show A1, A2
-    ccall((:SCOTCH_graphStat, libscotch), Void, 
+    ccall((:SCOTCH_graphStat, SCOTCH_LIBSCOTCH), Void, 
           (Ptr{Graph},
            Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cdouble},
            Ptr{Cint}, Ptr{Cint},            Ptr{Cdouble}, Ptr{Cdouble},
@@ -133,13 +134,13 @@ end
 # Strategy
 
 function stratAlloc()
-    strat = ccall((:SCOTCH_stratAlloc, libscotch), Ptr{Strat}, ())
+    strat = ccall((:SCOTCH_stratAlloc, SCOTCH_LIBSCOTCH), Ptr{Strat}, ())
     strat != C_NULL || error("Error in stratAlloc")
     return strat
 end
 
 function stratInit(strat::Ptr{Strat})
-    err = ccall((:SCOTCH_stratInit, libscotch), Cint,
+    err = ccall((:SCOTCH_stratInit, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Strat},),
                 strat)
     err == 0 || error("Error in stratInit")
@@ -147,7 +148,7 @@ function stratInit(strat::Ptr{Strat})
 end
 
 function stratGraphOrder(strat::Ptr{Strat}, st::String)
-    err = ccall((:SCOTCH_stratGraphOrder, libscotch), Cint,
+    err = ccall((:SCOTCH_stratGraphOrder, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Strat}, Ptr{Char}),
                 strat, pointer(st))
     err == 0 || error("Error in stratGraphOrder")
@@ -155,7 +156,7 @@ function stratGraphOrder(strat::Ptr{Strat}, st::String)
 end
 
 function stratExit(strat::Ptr{Strat})
-    ccall((:SCOTCH_stratExit, libscotch), Void,
+    ccall((:SCOTCH_stratExit, SCOTCH_LIBSCOTCH), Void,
           (Ptr{Strat},),
           strat)
 end
@@ -169,7 +170,7 @@ function graphOrder(graph::Ptr{Graph}, strat::Ptr{Strat})
     cblkptr = Array{Cint, 1}(1)
     rangtab = Array{Cint, 1}(nv + 1)
     treetab = Array{Cint, 1}(nv)
-    err = ccall((:SCOTCH_graphOrder, libscotch), Cint,
+    err = ccall((:SCOTCH_graphOrder, SCOTCH_LIBSCOTCH), Cint,
             (Ptr{Graph}, Ptr{Strat}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), 
             graph,       strat,      pointer(permtab), pointer(peritab), pointer(cblkptr), pointer(rangtab), pointer(treetab))
     err == 0 || error("Error in graphOrder")
@@ -177,13 +178,13 @@ function graphOrder(graph::Ptr{Graph}, strat::Ptr{Strat})
 end
 
 function orderAlloc()
-    optr = ccall((:SCOTCH_orderAlloc, libscotch), Ptr{Ordering}, ())
+    optr = ccall((:SCOTCH_orderAlloc, SCOTCH_LIBSCOTCH), Ptr{Ordering}, ())
     optr != C_NULL || error("Error in orderAlloc")
     return optr
 end
 
 function graphOrderInit(graph::Ptr{Graph}, ordering::Ptr{Ordering}, permtab::Array{Cint,1}, peritab::Array{Cint,1}, cblkptr::Array{Cint,1}, rangtab::Array{Cint,1}, treetab::Array{Cint,1})
-    err = ccall((:SCOTCH_graphOrderInit, libscotch), Cint,
+    err = ccall((:SCOTCH_graphOrderInit, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Graph}, Ptr{Ordering}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}),
                 graph, ordering, pointer(permtab), pointer(peritab), pointer(cblkptr), pointer(rangtab), pointer(treetab))
     err == 0 || error("Error in graphOrderInit")
@@ -191,7 +192,7 @@ function graphOrderInit(graph::Ptr{Graph}, ordering::Ptr{Ordering}, permtab::Arr
 end
 
 function graphOrderComputeList(graph::Ptr{Graph}, ordering::Ptr{Ordering}, listtab::Array{Cint,1}, strat::Ptr{Strat})
-    err = ccall((:SCOTCH_graphOrderComputeList, libscotch), Cint,
+    err = ccall((:SCOTCH_graphOrderComputeList, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Graph}, Ptr{Ordering}, Cint, Ptr{Cint}, Ptr{Strat}),
                 graph, ordering, Cint(length(listtab)), pointer(listtab), strat)
     err == 0 || error("Error in graphOrderComputeList")
@@ -199,7 +200,7 @@ function graphOrderComputeList(graph::Ptr{Graph}, ordering::Ptr{Ordering}, listt
 end
 
 function graphOrderCompute(graph::Ptr{Graph}, ordering::Ptr{Ordering}, strat::Ptr{Strat})
-    err = ccall((:SCOTCH_graphOrderCompute, libscotch), Cint,
+    err = ccall((:SCOTCH_graphOrderCompute, SCOTCH_LIBSCOTCH), Cint,
                 (Ptr{Graph}, Ptr{Ordering}, Ptr{Strat}),
                 graph, ordering, strat)
     err == 0 || error("Error in graphOrderCompute")
@@ -207,14 +208,14 @@ function graphOrderCompute(graph::Ptr{Graph}, ordering::Ptr{Ordering}, strat::Pt
 end
 
 function graphOrderCheck(graph::Ptr{Graph}, ordering::Ptr{Ordering})
-    err = ccall((:SCOTCH_graphOrderCheck, libscotch), Cint, 
+    err = ccall((:SCOTCH_graphOrderCheck, SCOTCH_LIBSCOTCH), Cint, 
                 (Ptr{Graph}, Ptr{Ordering}),
                 graph, ordering)
     err == 0 || error("Error in graphOrderCheck")
 end
 
 function graphOrderExit(graph::Ptr{Graph}, ordering::Ptr{Ordering})
-    ccall((:SCOTCH_graphOrderExit, libscotch), Void,
+    ccall((:SCOTCH_graphOrderExit, SCOTCH_LIBSCOTCH), Void,
                 (Ptr{Graph}, Ptr{Ordering}),
                 graph, ordering)
 end
